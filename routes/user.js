df5c48433dc0b6af5948427a0f47ce651e0e5800
reyhanfajarn13 <reyhanfajarn13@gmail.com>
2022-07-04 13:05:45 +0700

    recode the /routes/user.js

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,12 @@
+
 const express = require("express");
 
 const router = express.Router();
 
 //get data
 router.get('/',(req,res) => {
-    res.send("connected to server");
+    res.send("connected to server!");
 })
+
 
 module.exports = router;
